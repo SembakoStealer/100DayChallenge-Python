357283d32c8d2f5c0731de2fe76e6b427de3415f
Joyada Okomoto <2381038@unai.edu>
2025-02-22 15:07:18 +0700

    completed day 1 challenge

---
+++ Day 1/task/task.py
@@ -0,0 +1,7 @@
+userName = input("What is your name?\n")
+print("Hello " + userName + " , welcome to Band Name Generator ")
+
+userCityName = input("What city did you grew up in?\n")
+userPetName = input("What is the name of your pet?\n")
+userBandName = input("I see your band name should be... " + userCityName + " " + userPetName)
+
